b0cf2a1ba8228e6f845a894c90037a2dd0340956
万纯 <harrywan@webank.com>
2020-09-02 12:09:19 +0800

    修复文档问题，解决编译后的文档刷新会导致其他页面不能正常显示

--- packages/fes-doc/docs/.vuepress/enhanceApp.js
+++ packages/fes-doc/docs/.vuepress/enhanceApp.js
@@ -1,5 +1,6 @@
 // 使用异步函数也是可以的
 import "@webank/fes-ui/dist/styles/fes-ui.css";
+let installed = false;
 export default ({
     Vue, // VuePress 正在使用的 Vue 构造函数
     options, // 附加到根实例的一些选项
@@ -7,14 +8,20 @@ export default ({
     siteData, // 站点元数据
     isServer // 当前应用配置是处于 服务端渲染 或 客户端
 }) => {
-    Vue.mixin({
-        mounted(){
-            import("@webank/fes-ui").then(({ default: UiWebank }) => {
-                // install 组件库
-                Vue.use(UiWebank, {
-                    locale: "zh-cn"
-                });
-            });
+    return new Promise(function(resole, reject) {
+        if (!isServer) {
+            import("@webank/fes-ui")
+                .then(({ default: UiWebank }) => {
+                    installed = true;
+                    // install 组件库
+                    Vue.use(UiWebank, {
+                        locale: "zh-cn"
+                    });
+                    resole();
+                })
+                .catch(reject);
+        } else {
+            resole();
         }
-    })
+    });
 };
