47f67e54f1d2a2320d0842f5de16c7654c3e0b7f
harrywan <harrywan@webank.com>
2020-11-13 14:27:00 +0800

    fix(fes-core): 修复模糊匹配对"/"路由判断失效

--- packages/fes-core/src/util/index.js
+++ packages/fes-core/src/util/index.js
@@ -7,21 +7,22 @@ import event from './event';
 import history from './history';
 
 const util = {
-    // 验证一个path是否可以访问, 空的allowPage可以访问任何路由
+    // 验证一个path是否可以访问
     canRoute(path, allowPage) {
         path = path.split('?')[0];
         if (Array.isArray(allowPage) && allowPage.length > 0) {
-            if (path === '' && allowPage.includes('/')) return true;
-            if (path) {
-                for (let i = 0; i < allowPage.length; i++) {
-                    if (path === allowPage[i]) {
-                        return true;
-                    }
-                    // 支持*匹配
-                    const reg = new RegExp(`^${allowPage[i].replace('*', '.+')}$`);
-                    if (reg.test(path)) {
-                        return true;
-                    }
+            // 当路由为“/"时,传入的path为“”
+            if (path === '') {
+                path = '/';
+            }
+            for (let i = 0; i < allowPage.length; i++) {
+                if (path === allowPage[i]) {
+                    return true;
+                }
+                // 支持*匹配
+                const reg = new RegExp(`^${allowPage[i].replace('*', '.+')}$`);
+                if (reg.test(path)) {
+                    return true;
                 }
             }
         }
@@ -84,5 +85,13 @@ const util = {
     },
     _
 };
-objectUtil.merge(util, domUtil, objectUtil, typeUtil, { format }, { event }, { history });
-export default util;
+
+export default {
+    ...util,
+    ...domUtil,
+    ...objectUtil,
+    ...typeUtil,
+    format,
+    event,
+    history
+};
