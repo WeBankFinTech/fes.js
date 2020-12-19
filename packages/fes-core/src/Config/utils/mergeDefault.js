21f4f6650438e582ae884dce3f6a48c845d9b22a
bac-joker <haizekuo@gmail.com>
2020-12-19 16:31:12 +0800

    feat: rename Config to config

--- packages/fes-core/src/Config/utils/mergeDefault.js
+++
@@ -1,9 +0,0 @@
-import { deepmerge, lodash } from '@umijs/utils';
-
-
-export default ({ defaultConfig, config }) => {
-    if (lodash.isPlainObject(defaultConfig) && lodash.isPlainObject(config)) {
-        return deepmerge(defaultConfig, config);
-    }
-    return typeof config !== 'undefined' ? config : defaultConfig;
-};
