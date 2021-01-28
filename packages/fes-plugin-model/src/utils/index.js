9fd4fff3e2908dfaa9916e8b77aac39ff2b24306
万纯 <harrywan@webank.com>
2021-01-28 15:44:22 +0800

    fix: 优化plugin-model的错误日志

--- packages/fes-plugin-model/src/utils/index.js
+++ packages/fes-plugin-model/src/utils/index.js
@@ -141,7 +141,7 @@ export const genModels = (imports, absSrcPath) => {
     const models = sort(raw);
 
     if (checkDuplicates(contents.map(ele => ele.namespace))) {
-        throw Error('umi: models 中包含重复的 namespace！');
+        throw Error('plugin-model: models 中包含重复的 namespace！');
     }
     return raw.sort(
         (a, b) => models.indexOf(a.namespace) - models.indexOf(b.namespace)
