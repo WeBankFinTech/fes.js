df1d701012c42c344e53f186f201bc2c2528ca41
tianxuan <tianxuan@zuozhongyideMacBook-Pro.local>
2021-03-05 15:38:38 +0800

    feat: 新增配置的相关文档

--- docs/.vuepress/configs/sidebar/zh.ts
+++ docs/.vuepress/configs/sidebar/zh.ts
@@ -15,6 +15,7 @@ export const zh: SidebarConfig = {
       text: '基础',
       children: [
         '/zh/guide/directory-structure.md', 
+        '/zh/guide/config.md', 
         '/zh/guide/route.md', 
         '/zh/guide/plugin.md', 
         '/zh/guide/template.md', 
