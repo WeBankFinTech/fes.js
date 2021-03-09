cb69a05f296f6f958e50d1fa0fba38b374840dff
bac-joker <haizekuo@gmail.com>
2021-03-09 19:12:36 +0800

    fix: request 参数丢失问题

--- packages/fes-plugin-request/src/template/paramsProcess.js
+++ packages/fes-plugin-request/src/template/paramsProcess.js
@@ -3,9 +3,9 @@ import { checkHttpRequestHasBody, trimObj } from './helpers';
 export default async (ctx, next) => {
     const config = ctx.config;
     if (checkHttpRequestHasBody(config.method)) {
-        config.data = trimObj(config.data);
+        trimObj(config.data);
     } else {
-        config.params = trimObj(config.params);
+        trimObj(config.params);
     }
     await next();
 };
