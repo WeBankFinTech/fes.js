3940f238e4c1addc6bfbf75d10b234e0dad82e69
bac-joker <haizekuo@gmail.com>
2021-03-12 19:34:06 +0800

    fix: request plugin data adaptor error

--- packages/fes-plugin-request/src/template/resDataAdaptor.js
+++ packages/fes-plugin-request/src/template/resDataAdaptor.js
@@ -1,7 +1,7 @@
 import { isFunction, isObject, isString } from './helpers';
 
 export default async ({ response, responseDataAdaptor }, next) => {
-    if (isFunction(responseDataAdaptor) && (isObject(response.data) || isString(response.data))) {
+    if (isFunction(responseDataAdaptor) && response && (isObject(response.data) || isString(response.data))) {
         response.data = responseDataAdaptor(response.data);
     }
     await next();
