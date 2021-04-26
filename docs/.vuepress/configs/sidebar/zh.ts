2dc949f9ca211adc3e5d5081c7d49a7ed4b0f33b
harrywan <harrywan@webank.com>
2021-04-26 17:28:41 +0800

    docs: plugin-sass

--- docs/.vuepress/configs/sidebar/zh.ts
+++ docs/.vuepress/configs/sidebar/zh.ts
@@ -7,22 +7,22 @@ export const zh: SidebarConfig = {
       text: '介绍',
       children: [
         '/zh/guide/README.md',
-        '/zh/guide/getting-started.md', 
+        '/zh/guide/getting-started.md',
       ],
     },
     {
       isGroup: true,
       text: '基础',
       children: [
-        '/zh/guide/directory-structure.md', 
-        '/zh/guide/config.md', 
-        '/zh/guide/runtime-config.md', 
-        '/zh/guide/route.md', 
-        '/zh/guide/plugin.md', 
-        '/zh/guide/template.md', 
-        '/zh/guide/mock.md', 
-        '/zh/guide/env.md', 
-        '/zh/guide/css.md', 
+        '/zh/guide/directory-structure.md',
+        '/zh/guide/config.md',
+        '/zh/guide/runtime-config.md',
+        '/zh/guide/route.md',
+        '/zh/guide/plugin.md',
+        '/zh/guide/template.md',
+        '/zh/guide/mock.md',
+        '/zh/guide/env.md',
+        '/zh/guide/css.md',
       ]
     },
     {
@@ -61,6 +61,7 @@ export const zh: SidebarConfig = {
         '/zh/reference/plugin/plugins/request.md',
         '/zh/reference/plugin/plugins/vuex.md',
         '/zh/reference/plugin/plugins/qiankun.md',
+        '/zh/reference/plugin/plugins/sass.md',
       ],
     },
     {
---
+++ docs/reference/plugin/plugins/sass.md
@@ -0,0 +1,27 @@
+# @fesjs/plugin-access
+
+
+
+## 介绍
+Fes.js 默认只支持 `less`，通过此插件扩展支持 `sass`
+
+## 启用方式
+在 `package.json` 中引入依赖：
+```json
+{
+    "dependencies": {
+        "@fesjs/fes": "^2.0.0",
+        "@fesjs/plugin-sass": "^2.0.0"
+    },
+}
+```
+
+## global css
+添加 `src/global.scss` 和 `src/global.sass` 为全局CSS入口，添加一些通用样式内容。
+
+## Vue 单文件组件
+Vue 单文件组件的 `<style></style>` 添加 `lang='scss'`，例如：
+```vue
+<style lang="scss">
+</style>
+``` 
