95d5b4bcd9b614ffb0aa3372fb8f27eb7f8c9060
winixt <haizekuo@gmail.com>
2021-11-18 14:58:18 +0800

    fix: 移除可有可无代码

--- packages/fes-plugin-windicss/windi.config.js
+++ packages/fes-plugin-windicss/windi.config.js
@@ -1,7 +1,7 @@
 export default {
     extract: {
         // A common use case is scanning files from the root directory
-        include: ['**/*.{vue,jsx,tsx}'],
+        include: ['**/*.{vue,jsx,js,ts,tsx}'],
         // if you are excluding files, make sure you always include node_modules and .git
         exclude: ['node_modules', '.git', 'dist']
     }
