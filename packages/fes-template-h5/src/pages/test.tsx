5b1553ef5856b56b96ae8b12e3ddd1fb478a7a32
harrywan <445436867@qq.com>
2022-03-09 10:27:11 +0800

    feat: 页面支持tsx，提供defineRoute配置tsx和jsx的route (#106)
    * feat: 页面支持tsx，提供defineRoute配置tsx和jsx的route
    
    * .vue文件也支持defineRouteMeta
    
    * feat: 改回声明用法

---
+++ packages/fes-template-h5/src/pages/test.tsx
@@ -0,0 +1,15 @@
+import { defineRouteMeta, useRoute } from '@fesjs/fes';
+import { defineComponent } from 'vue';
+
+defineRouteMeta({
+    title: 'test',
+    name: 'test'
+})
+
+export default defineComponent({
+    setup() {
+        const route = useRoute();
+        console.log(route)
+        return () => <div>hello tsx</div>;
+    }
+});
