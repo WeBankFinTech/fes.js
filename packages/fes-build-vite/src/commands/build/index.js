f9397495c49d9e9047111b79296a35a19e26e357
winixt <haizekuo@gmail.com>
2022-04-03 20:00:24 +0800

    chore: 完成部分 vite 构建相关

--- packages/fes-build-vite/src/commands/build/index.js
+++ packages/fes-build-vite/src/commands/build/index.js
@@ -1,11 +1,42 @@
-/**
- * outputPath
- * inlineLimit: 如何实现
- * publicPath: 就是 base
- * polyfill： @vitejs/plugin-legacy
- * 确认 css 最终构建实现 autoprefixer postcss-safe-parser postcss-flexbugs-fixes
- */
+import { build } from 'vite';
+import { existsSync } from 'fs';
+import getBuildConfig from './getBuildConfig';
 
-export default () => {
-    // console.log(api, 'TODO: 实现 vite build');
-};
+export default function (api) {
+    const {
+        paths,
+        utils: { rimraf },
+    } = api;
+
+    api.registerCommand({
+        command: 'build',
+        description: 'build application for production',
+        async fn() {
+            rimraf.sync(paths.absTmpPath);
+
+            // generate files
+            await api.applyPlugins({
+                key: 'onGenerateFiles',
+                type: api.ApplyPluginsType.event,
+            });
+
+            const bundleConfig = await getBuildConfig(api);
+            try {
+                // clear output path before exec build
+                if (process.env.CLEAR_OUTPUT !== 'none') {
+                    if (paths.absOutputPath && existsSync(paths.absOutputPath)) {
+                        rimraf.sync(paths.absOutputPath);
+                    }
+                }
+
+                await build(bundleConfig);
+                if (process.env.RM_TMPDIR !== 'none') {
+                    rimraf.sync(paths.absTmpPath);
+                }
+            } catch (err) {
+                // throw build error
+                throw err;
+            }
+        },
+    });
+}
