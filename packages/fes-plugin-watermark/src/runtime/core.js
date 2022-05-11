70b034a3513b56f6814ed89d0f84ee18733beba2
ocean_gao <gaopandyx@163.com>
2022-05-11 10:32:44 +0800

    feat:  plugin-watermark 提供 destroyWatermark (#124)
    * feat: 水印支持动态关闭和打开
    
    * refactor: watermark代码规范
    
    * feat: 水印关闭逻辑优化
    
    * fix: watermark 导出 destroyWatermark
    
    * refactor: 代码规范
    
    * feat: 水印性能优化
    
    * refactor: 代码规范
    
    * refactor: 代码规范
    
    * fix: 重新渲染水印

--- packages/fes-plugin-watermark/src/runtime/core.js
+++ packages/fes-plugin-watermark/src/runtime/core.js
@@ -30,26 +30,27 @@ function timeFormat(date, format = 'YYYY-MM-DD') {
     return format.replace(/Y+|M+|D+|H+|h+|m+|s+|S+|Q/g, str => String(map[str]));
 }
 
-// canvas 实现 watermark
-export function createWatermark({
-    container = document.body,
-    width = 300,
-    height = 300,
-    textAlign = 'center',
-    textBaseline = 'middle',
-    fontSize = '14px',
-    fontFamily = 'Microsoft Yahei',
-    fillStyle = 'rgba(184, 184, 184, 0.3)',
-    content = '请勿外传',
-    rotate = 25,
-    zIndex = 99999,
-    timestamp = 'YYYY-MM-DD HH:mm'
-} = {}) {
-    // eslint-disable-next-line no-undef
-    if (WATERMARK_DISABLED) {
-        return;
-    }
-    const param = {
+const defaultOption = {
+    content: '请勿外传',
+    container: document.body,
+    width: 300,
+    height: 300,
+    textAlign: 'center',
+    textBaseline: 'middle',
+    fontSize: '14px',
+    fontFamily: 'Microsoft Yahei',
+    fillStyle: 'rgba(184, 184, 184, 0.3)',
+    rotate: 25,
+    zIndex: 99999,
+    timestamp: 'YYYY-MM-DD HH:mm'
+};
+
+let _wmMo = null; // MutationObserver
+let _wmTimer = null; // timestamp
+
+function _createWatermark(param) {
+    const {
+        content,
         container,
         width,
         height,
@@ -58,11 +59,11 @@ export function createWatermark({
         fontSize,
         fontFamily,
         fillStyle,
-        content,
         rotate,
         zIndex,
         timestamp
-    };
+    } = param;
+
     const canvas = document.createElement('canvas');
     canvas.setAttribute('width', `${width}px`);
     canvas.setAttribute('height', `${height}px`);
@@ -108,17 +109,17 @@ export function createWatermark({
 
     const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
     if (MutationObserver) {
-        let mo = new MutationObserver(() => {
+        _wmMo = new MutationObserver(() => {
             __wm = document.querySelector('.__wm');
             if ((__wm && __wm.getAttribute('style') !== styleStr) || !__wm) {
                 // 避免一直触发
-                mo.disconnect();
-                mo = null;
-                createWatermark(param);
+                _wmMo.disconnect();
+                _wmMo = null;
+                _createWatermark(param);
             }
         });
 
-        mo.observe(container, {
+        _wmMo.observe(container, {
             attributes: true,
             subtree: true,
             childList: true
@@ -135,9 +136,38 @@ export function createWatermark({
             timeout = 1000 * 60 * 60;
         }
 
-        setTimeout(() => {
-            // 触发MutationObserver
+        _wmTimer = window.setTimeout(() => {
+            // 触发 MutationObserver
             watermarkDiv.style.bottom = '0';
         }, timeout);
     }
+}
+
+// 销毁水印
+export function destroyWatermark() {
+    // 监听器关闭
+    _wmMo && _wmMo.disconnect();
+    _wmMo = null;
+    _wmTimer && window.clearTimeout(_wmTimer);
+    _wmTimer = null;
+
+    // 删除水印元素
+    const __wm = document.querySelector('.__wm');
+    __wm && __wm.parentNode.removeChild(__wm);
+}
+
+// canvas 实现 watermark
+export function createWatermark(option) {
+    // eslint-disable-next-line no-undef
+    if (WATERMARK_DISABLED) {
+        return;
+    }
+
+    // 为避免多次调用 createWatermark 触发重复监听，这里先执行销毁水印操作
+    destroyWatermark();
+
+    _createWatermark({
+        ...defaultOption,
+        ...option
+    });
 }
