9835d951b62ce6eeba7a5d5b04a5addd079a59b6
winixt <haizekuo@gmail.com>
2022-08-17 11:02:50 +0800

    chore: 升级 vue 依赖到最新版本，处理 compile-sfc 和 vue 版本冲突问题

--- packages/fes-preset-built-in/src/index.js
+++ packages/fes-preset-built-in/src/index.js
@@ -18,7 +18,6 @@ export default function () {
             require.resolve('./plugins/features/chainWebpack'),
             require.resolve('./plugins/features/cssLoader'),
             require.resolve('./plugins/features/copy'),
-            require.resolve('./plugins/features/checkVuePackage'),
             require.resolve('./plugins/features/define'),
             require.resolve('./plugins/features/devServer'),
             require.resolve('./plugins/features/devtool'),
