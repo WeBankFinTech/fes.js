d9a65d03586a03b4a3529d6258eca5e8a5834139
winixt <haizekuo@gmail.com>
2022-08-26 20:22:04 +0800

    chore: 优化targets

--- packages/fes-preset-built-in/src/plugins/features/targets.js
+++ packages/fes-preset-built-in/src/plugins/features/targets.js
@@ -3,15 +3,8 @@ export default (api) => {
         key: 'targets',
         config: {
             default: {
-                android: '61.0.0',
-                chrome: '61.0.0',
-                edge: '16.0.0',
-                firefox: '60.0.0',
-                ios: '10.3.0',
-                node: '13.2.0',
-                opera: '48.0.0',
-                safari: '10.1.0',
-                samsung: '8.2.0',
+                chrome: '64',
+                ios: '11',
                 browsers: ['defaults and not chrome < 61'],
             },
             schema(joi) {
