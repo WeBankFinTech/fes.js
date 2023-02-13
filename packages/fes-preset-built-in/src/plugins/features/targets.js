0319b4f3bc8d75ca62c65b9fb152eb64dfca2286
RiESAEX <2597245950@qq.com>
2023-02-13 08:17:29 +0000

    fix: keep browsers

--- packages/fes-preset-built-in/src/plugins/features/targets.js
+++ packages/fes-preset-built-in/src/plugins/features/targets.js
@@ -5,7 +5,7 @@ export default (api) => {
             default: {
                 chrome: '64',
                 ios: '11',
-                // browsers: ['defaults and not chrome < 61'],
+                browsers: ['defaults and not chrome < 61'],
             },
             schema(joi) {
                 return joi.object();
