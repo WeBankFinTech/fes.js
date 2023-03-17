64d57fdea10c8300c412d4439004ca9e519015d3
qlin <haizekuo@gmail.com>
2023-03-17 21:05:36 +0800

    fix: 解决 webpack 安全漏洞问题 (#176)

--- packages/fes-preset-built-in/src/plugins/features/publicPath.js
+++ packages/fes-preset-built-in/src/plugins/features/publicPath.js
@@ -5,9 +5,7 @@ export default (api) => {
             default: '/',
             schema(joi) {
                 return joi
-                    .string()
-                    .regex(/\/$/)
-                    .error(new Error('config.publicPath must end with /.'));
+                    .string();
             }
         }
     });
