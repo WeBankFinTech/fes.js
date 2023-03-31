bafa4d08cc88290451191604e9289ab1e8915596
wanchun <445436867@qq.com>
2023-03-31 14:38:43 +0800

    feat: 优化peer依赖

--- packages/fes-preset-built-in/src/utils/constants.js
+++ packages/fes-preset-built-in/src/utils/constants.js
@@ -1,4 +1,4 @@
-import { winPath } from '@fesjs/utils';
 import { dirname } from 'path';
+import { winPath } from '@fesjs/utils';
 
 export const runtimePath = winPath(dirname(require.resolve('@fesjs/runtime/package.json')));
