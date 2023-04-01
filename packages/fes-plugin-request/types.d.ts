d9ba873a92d8494325ffa6c1b7507bd5d6403491
winixt <haizekuo@gmail.com>
2023-04-01 15:59:37 +0800

    feat: change request to fetch

--- packages/fes-plugin-request/types.d.ts
+++ packages/fes-plugin-request/types.d.ts
@@ -1,39 +1,18 @@
-import { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
+import type { Config, ParamsType } from '@qlin/request/dist/interface';
 import { Ref } from 'vue';
 
-type RequestInterceptor = (value: AxiosRequestConfig) => AxiosRequestConfig | [(value: AxiosRequestConfig) => AxiosRequestConfig, (error: any) => any];
-type ResponseInterceptor = (value: AxiosResponse) => AxiosResponse | [(value: AxiosResponse) => AxiosResponse, (error: any) => any];
+export * from '@qlin/request';
 
-export type RequestResponse = AxiosResponse;
-export type RequestError = AxiosError & { type: string; msg: string; [key: string]: any };
-interface RequestPluginOption {
-    mergeRequest?: boolean;
-    dataHandler?(data: any, response: AxiosResponse): any;
-    errorHandler?(error: RequestError): void;
-    cache?:
-        | boolean
-        | {
-              cacheType?: 'ram' | 'sessionStorage' | 'localStorage';
-              cacheTime?: number;
-          };
-}
-
-export type RequestOptions = AxiosRequestConfig & RequestPluginOption;
-
-export function request(url: string, data?: null | Record<string, any>, options?: AxiosRequestConfig & RequestPluginOption): Promise<any>;
+export function request(url: string, data?: ParamsType | null, options?: Partial<Config>): Promise<any>;
+export function rawRequest(url: string, data?: ParamsType | null, options?: Partial<Config>): Promise<any>;
 export function useRequest(
     url: string,
-    data?: null | Record<string, any>,
-    options?: AxiosRequestConfig & RequestPluginOption,
+    data?: null | ParamsType,
+    options?: Partial<Config>,
 ): { loadingRef: Ref<boolean>; errorRef: Ref<Error>; dataRef: Ref<any> };
 
 declare module '@fesjs/fes' {
     interface PluginRuntimeConfig {
-        request?: {
-            dataHandler?(data: any, response: AxiosResponse): any;
-            errorHandler?(error: RequestError): void;
-            requestInterceptors?: RequestInterceptor[];
-            responseInterceptors?: ResponseInterceptor[];
-        } & AxiosRequestConfig;
+        request?: Partial<Config>;
     }
 }
