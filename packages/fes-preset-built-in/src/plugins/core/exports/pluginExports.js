798dd77cf863b191fb5690d960b85c53ce8c2bf5
听海 <445436867@qq.com>
2023-04-07 20:46:33 +0800

    feat: 添加[...slug].vue方式替换*.vue实现模糊匹配 (#183)

--- packages/fes-preset-built-in/src/plugins/core/exports/pluginExports.js
+++ packages/fes-preset-built-in/src/plugins/core/exports/pluginExports.js
@@ -1,11 +1,11 @@
-import generateExports from '../../../../utils/generateExports';
+import generateExports from '../../../utils/generateExports';
 
 export default function (api) {
     api.onGenerateFiles(async () => {
         const fesExports = await api.applyPlugins({
             key: 'addPluginExports',
             type: api.ApplyPluginsType.add,
-            initialValue: []
+            initialValue: [],
         });
 
         const fesExportsHook = {}; // repeated definition
@@ -13,11 +13,13 @@ export default function (api) {
         api.writeTmpFile({
             path: absoluteFilePath,
             content: `${fesExports
-                .map(item => generateExports(absoluteFilePath, {
-                    item,
-                    fesExportsHook
-                }))
-                .join('\n')}\n`
+                .map((item) =>
+                    generateExports(absoluteFilePath, {
+                        item,
+                        fesExportsHook,
+                    }),
+                )
+                .join('\n')}\n`,
         });
     });
 }
