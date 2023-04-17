92c9d402e45d95b0ed320eff5d0dacc21f42e462
wanchun <445436867@qq.com>
2023-04-17 06:38:35 +0000

    Deploying to gh-pages from @ WeBankFinTech/fes.js@82a9444f09eabc33fed4ebe73d5eff8d63ccf6ea 🚀

---
+++ assets/index.html.1f2b7059.js
@@ -0,0 +1,17 @@
+import{_ as i,r as a,o as r,c as d,a as s,w as t,b as e,d as n}from"./app.005e0daf.js";const u={},m=e("h2",{id:"像数-1-2-3-一样容易",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#像数-1-2-3-一样容易","aria-hidden":"true"},"#"),n(" 像数 1, 2, 3 一样容易")],-1),p=e("div",{class:"language-bash line-numbers-mode","data-ext":"sh"},[e("pre",{class:"language-bash"},[e("code",null,[e("span",{class:"token comment"},"# 创建模板"),n(`
+`),e("span",{class:"token function"},"pnpm"),n(` create @fesjs/fes-app myapp
+
+`),e("span",{class:"token comment"},"# 安装依赖"),n(`
+`),e("span",{class:"token function"},"pnpm"),n(` i
+
+`),e("span",{class:"token comment"},"# 运行"),n(`
+`),e("span",{class:"token function"},"pnpm"),n(` dev
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),h=e("div",{class:"language-bash line-numbers-mode","data-ext":"sh"},[e("pre",{class:"language-bash"},[e("code",null,[e("span",{class:"token comment"},"# 创建模板"),n(`
+npx @fesjs/create-fes-app myapp
+
+`),e("span",{class:"token comment"},"# 安装依赖"),n(`
+`),e("span",{class:"token function"},"npm"),n(),e("span",{class:"token function"},"install"),n(`
+
+`),e("span",{class:"token comment"},"# 运行"),n(`
+`),e("span",{class:"token function"},"npm"),n(` run dev
+`)])]),e("div",{class:"line-numbers","aria-hidden":"true"},[e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"}),e("div",{class:"line-number"})])],-1),_=e("h2",{id:"反馈",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#反馈","aria-hidden":"true"},"#"),n(" 反馈")],-1),b=e("thead",null,[e("tr",null,[e("th",null,"Github Issue"),e("th",null,"Fes.js 开源运营小助手")])],-1),f={href:"https://github.com/WeBankFinTech/fes.js/issues",target:"_blank",rel:"noopener noreferrer"},v=e("td",null,[e("img",{src:"https://cos-1254145788.cos.ap-guangzhou.myqcloud.com/WechatIMG104.jpeg",height:"250"})],-1);function k(g,x){const l=a("CodeGroupItem"),c=a("CodeGroup"),o=a("ExternalLinkIcon");return r(),d("div",null,[m,s(c,null,{default:t(()=>[s(l,{title:"PNPM",active:""},{default:t(()=>[p]),_:1}),s(l,{title:"NPM"},{default:t(()=>[h]),_:1})]),_:1}),_,e("table",null,[b,e("tbody",null,[e("tr",null,[e("td",null,[e("a",f,[n("@fesjs/fes.js/issues"),s(o)])]),v])])])])}const C=i(u,[["render",k],["__file","index.html.vue"]]);export{C as default};
