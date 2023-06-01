e271094414a4cc1074589c8a516fb2381cb11477
winixt <haizekuo@gmail.com>
2023-06-01 21:25:21 +0800

    fix: 修复全局样式加载顺序问题

--- packages/fes-preset-built-in/src/plugins/features/globalCSS.js
+++ packages/fes-preset-built-in/src/plugins/features/globalCSS.js
@@ -2,7 +2,16 @@ import { relative, join } from 'path';
 import { existsSync } from 'fs';
 
 export default (api) => {
-    const { paths } = api;
+    api.describe({
+        key: 'globalCSS',
+        config: {
+            schema(joi) {
+                return joi.string();
+            },
+        },
+        default: '',
+    });
+    const { paths, utils } = api;
     const { absSrcPath = '', absTmpPath = '' } = paths;
     const files = ['global.css', 'global.less', 'global.scss', 'global.sass', 'global.styl', 'global.stylus'];
     const globalCSSFile = files
@@ -10,10 +19,24 @@ export default (api) => {
         .filter((file) => existsSync(file))
         .slice(0, 1);
 
+    const isBeforeImports = () => api.config.globalCSS === 'beforeImports';
+
     if (globalCSSFile.length) {
         api.addEntryImportsAhead({
             stage: 1,
-            fn: () => [{ source: relative(absTmpPath, globalCSSFile[0]) }],
+            fn: () => {
+                if (isBeforeImports()) {
+                    return [{ source: relative(absTmpPath, globalCSSFile[0]) }];
+                }
+                return [];
+            },
         });
     }
+
+    api.addEntryCodeAhead(() => {
+        if (!isBeforeImports()) {
+            return `${globalCSSFile.map((file) => `import '${utils.winPath(relative(absTmpPath, file))}';`).join('')}`;
+        }
+        return [];
+    });
 };
