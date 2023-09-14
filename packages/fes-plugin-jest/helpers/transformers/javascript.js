33b76940d85d0c0d5f1936c46837c14adf64bfae
听海 <445436867@qq.com>
2023-09-14 14:15:10 +0800

    fix: 处理create-fes-app中丢失.npmrc问题 & 升级plugin-jest支持tsx? (#216)

--- packages/fes-plugin-jest/helpers/transformers/javascript.js
+++ packages/fes-plugin-jest/helpers/transformers/javascript.js
@@ -1,10 +1,8 @@
 const babelJest = require('babel-jest').default;
 
 module.exports = babelJest.createTransformer({
-    presets: [
-        ['@babel/preset-env', { targets: { node: 'current' } }]
-    ],
+    presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
     plugins: ['@vue/babel-plugin-jsx'],
     babelrc: false,
-    configFile: false
+    configFile: false,
 });
--- packages/fes-plugin-jest/src/createDefaultConfig.js
+++ packages/fes-plugin-jest/src/createDefaultConfig.js
@@ -9,8 +9,8 @@ export default (cwd, args) => {
     const hasSrc = existsSync(join(cwd, 'src'));
     return {
         collectCoverageFrom: [
-            'index.{js,jsx,vue}',
-            hasSrc && 'src/**/*.{js,jsx,vue}',
+            'index.{js,ts,jsx,tsx,vue}',
+            hasSrc && 'src/**/*.{js,ts,jsx,tsx,vue}',
             '!**/.fes/**',
             '!**/typings/**',
             '!**/types/**',
@@ -21,16 +21,25 @@ export default (cwd, args) => {
         moduleFileExtensions: [
             'js',
             'jsx',
+            'ts',
+            'tsx',
             'json',
             // tell Jest to handle *.vue files
             'vue',
         ],
         transform: {
-            // process *.vue files with vue-jest
-            '^.+\\.vue$': require.resolve('vue3-jest'),
             '.+\\.(css|styl|less|sass|scss|jpg|jpeg|png|svg|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
                 require.resolve('jest-transform-stub'),
+            // process *.vue files with vue-jest
+            '^.+\\.vue$': require.resolve('@vue/vue3-jest'),
             '^.+\\.jsx?$': require.resolve('../helpers/transformers/javascript'),
+            // process *.ts files with ts-jest
+            '^.+\\.tsx?$': [
+                require.resolve('ts-jest'),
+                {
+                    // ts-jest configuration goes here
+                },
+            ],
         },
         transformIgnorePatterns: ['/node_modules/'],
         // support the same @ -> src alias mapping in source code
@@ -39,7 +48,9 @@ export default (cwd, args) => {
         },
         testMatch: [`**/tests/**/*.(${testMatchTypes.join('|')}).[jt]s?(x)`, '**/__tests__/**/*.[jt]s?(x)'],
         // https://github.com/facebook/jest/issues/6766
-        testURL: 'http://localhost/',
+        testEnvironmentOptions: {
+            url: 'http://localhost/',
+        },
         watchPlugins: [require.resolve('jest-watch-typeahead/filename'), require.resolve('jest-watch-typeahead/testname')],
         verbose: true,
     };
