97220bac4627a43a091a46c4bec991222ac3c936
qlin <haizekuo@gmail.com>
2023-10-24 18:34:53 +0800

    fix: 优化依赖 (#220)

--- packages/fes-template-h5/.fes.js
+++ packages/fes-template-h5/.fes.js
@@ -1,5 +1,5 @@
 // fes.config.js 只负责管理 cli 相关的配置
-import pxtoviewport from '@ttou/postcss-px-to-viewport';
+import pxtoviewport from 'postcss-px-to-viewport-8-plugin';
 import { defineBuildConfig } from '@fesjs/fes';
 
 export default defineBuildConfig({
