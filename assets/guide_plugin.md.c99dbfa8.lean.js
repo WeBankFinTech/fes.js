fe649c56049da41472d1d8badf9428a2b7ae3070
wanchun <445436867@qq.com>
2023-10-24 10:36:15 +0000

    Deploying to gh-pages from @ WeBankFinTech/fes.js@97220bac4627a43a091a46c4bec991222ac3c936 🚀

--- assets/guide_plugin.md.c99dbfa8.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o as a,c as e,V as o}from"./chunks/framework.c1e1f082.js";const C=JSON.parse('{"title":"插件","description":"","frontmatter":{},"headers":[],"relativePath":"guide/plugin.md"}'),n={name:"guide/plugin.md"},l=o("",29),p=[l];function c(t,r,i,d,D,y){return a(),e("div",null,p)}const u=s(n,[["render",c]]);export{C as __pageData,u as default};
