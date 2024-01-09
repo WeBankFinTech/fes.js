2bb6903bb302634796c0e0689baa02579a683d80
yqllin <524123601@qq.com>
2024-01-09 16:50:23 +0800

    feat: 升级 eslint 配置及版本

--- eslint.config.js
+++ eslint.config.js
@@ -2,18 +2,35 @@
 import antfu from '@antfu/eslint-config';
 
 export default await antfu({
+    files: ['**/*.js', '**/*.jsx', '**/*.vue', '**/*.ts'],
+    // TODO: 使用 ignore 代替 cli 命令中的配置
     stylistic: {
-        indent: 4, // 4, or 'tab'
-        quotes: 'single', // or 'double'
+        indent: 4,
+        quotes: 'single',
         semi: 'always',
     },
     typescript: true,
     vue: true,
     rules: {
+        'curly': ['error', 'multi-line'],
         'vue/block-order': [
             'error',
             {
                 order: ['template', 'script', 'style'],
+            },
+        ],
+        'style/member-delimiter-style': [
+            'error',
+            {
+                multiline: {
+                    delimiter: 'semi',
+                    requireLast: true,
+                },
+                singleline: {
+                    delimiter: 'semi',
+                    requireLast: false,
+                },
+                multilineDetection: 'brackets',
             },
         ],
     },
