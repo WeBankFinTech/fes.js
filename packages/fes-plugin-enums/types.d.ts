cda3196e7169a002741f511cf5cb5a155f5c0a23
qlin <haizekuo@gmail.com>
2024-02-19 16:14:28 +0800

    fix: 优化类型 (#231)

--- packages/fes-plugin-enums/types.d.ts
+++ packages/fes-plugin-enums/types.d.ts
@@ -7,11 +7,11 @@ interface EnumExtend {
 }
 
 interface EnumApi {
-    get(name: string, key?: string, extend?: EnumExtend): any;
-    push(name: string, _enum?: [] | object, option?: { keyName: string; valueName: string }): any;
-    remove(name: string): void;
-    concat(name: string, _enum: [] | object, option?: { keyName: string; valueName: string; before: boolean; extend: EnumExtend }): any;
-    convert(name: string, _enum?: [] | object, option?: { keyName: string; valueName: string }): any;
+    get: (name: string, key?: string, extend?: EnumExtend) => any;
+    push: (name: string, _enum?: [] | object, option?: { keyName: string; valueName: string }) => any;
+    remove: (name: string) => void;
+    concat: (name: string, _enum: [] | object, option?: { keyName: string; valueName: string; before: boolean; extend: EnumExtend }) => any;
+    convert: (name: string, _enum?: [] | object, option?: { keyName: string; valueName: string }) => any;
 }
 
 export const enums: EnumApi;
