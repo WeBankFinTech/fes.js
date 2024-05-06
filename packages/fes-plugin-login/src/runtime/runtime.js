c7e7efaa316986bbd65d64fc9b6ce2102fa792ca
qlin <haizekuo@gmail.com>
2024-05-06 15:14:26 +0800

    fix: login 兼容不同的 request 版本 (#242)
    * fix: login 兼容不同的 request 版本
    
    * chore: upgrade request

--- packages/fes-plugin-login/src/runtime/runtime.js
+++ packages/fes-plugin-login/src/runtime/runtime.js
@@ -1,8 +1,12 @@
+import { request } from '@@/core/pluginExports';
 import { ApplyPluginsType, getRouter, plugin } from '@fesjs/fes';
 
 let config;
 function getLoginConfig() {
-    if (config) return config;
+    if (config) {
+        return config;
+    }
+
     config = plugin.applyPlugins({
         key: 'login',
         type: ApplyPluginsType.modify,
@@ -14,38 +18,63 @@ function getLoginConfig() {
     return config;
 }
 
+const defaultExport = {
+    onRouterCreated({ router }) {
+        const { hasLogin, loginPath } = getLoginConfig();
+        if (hasLogin && loginPath) {
+            let isAuthenticated;
+            router.beforeEach(async (to, from, next) => {
+                if (to.path !== loginPath && !isAuthenticated) {
+                    isAuthenticated = await hasLogin();
+                    if (!isAuthenticated) {
+                        return next({ path: loginPath });
+                    }
+                }
+                next();
+            });
+        }
+    },
+};
+
 // ACCESS
 
-export function request(memo) {
-    if (!memo.responseInterceptors) {
-        memo.responseInterceptors = [];
-    }
-    memo.responseInterceptors.push([
-        (response) => response,
-        (error) => {
+if (request.version) {
+    defaultExport.request = (memo) => {
+        const config = getRunTimeConfig();
+        if (config.ignore401Redirect) {
+            return memo;
+        }
+
+        const errorHandler = memo.errorHandler;
+        memo.errorHandler = (error) => {
             if (error?.response?.status === 401) {
                 const router = getRouter();
                 const { loginPath } = getLoginConfig();
                 router.push({ path: loginPath });
             }
-            throw error;
-        },
-    ]);
-    return memo;
+            errorHandler && errorHandler(error);
+        };
+        return memo;
+    };
+}
+else {
+    defaultExport.request = (memo) => {
+        if (!memo.responseInterceptors) {
+            memo.responseInterceptors = [];
+        }
+        memo.responseInterceptors.push([
+            response => response,
+            (error) => {
+                if (error?.response?.status === 401) {
+                    const router = getRouter();
+                    const { loginPath } = getLoginConfig();
+                    router.push({ path: loginPath });
+                }
+                throw error;
+            },
+        ]);
+        return memo;
+    };
 }
 
-export function onRouterCreated({ router }) {
-    const { hasLogin, loginPath } = getLoginConfig();
-    if (hasLogin && loginPath) {
-        let isAuthenticated;
-        router.beforeEach(async (to, from, next) => {
-            if (to.path !== loginPath && !isAuthenticated) {
-                isAuthenticated = await hasLogin();
-                if (!isAuthenticated) {
-                    return next({ path: loginPath });
-                }
-            }
-            next();
-        });
-    }
-}
+export default defaultExport;
