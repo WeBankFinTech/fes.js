35328603c6379b8bb075c293780ccd860aac9289
听海 <445436867@qq.com>
2024-09-18 14:09:24 +0800

    refactor: 完善useLayout (#250)

--- packages/fes-plugin-layout/src/runtime/useLayout.js
+++ packages/fes-plugin-layout/src/runtime/useLayout.js
@@ -1,12 +1,31 @@
-import { createSharedComposable } from '@vueuse/core';
-import { shallowReactive } from 'vue';
+import { inject, ref } from 'vue';
 
-function _useLayout() {
-    const state = shallowReactive({
-        closeTab: () => {},
-    });
+import { useRoute } from '@@/core/coreExports';
 
-    return state;
+export const PLUGIN_LAYOUT_TITLE_KEY = Symbol('PLUGIN_LAYOUT_TITLE_KEY');
+
+export const PLUGIN_LAYOUT_KEY = Symbol('PLUGIN_LAYOUT_KEY');
+
+export function useTabTitle(title) {
+    const titleMap = inject(PLUGIN_LAYOUT_TITLE_KEY);
+    if (!titleMap) {
+        console.warn('[plugin-layout]: 未正确获取到titleMap');
+        return;
+    }
+    const route = useRoute();
+    const titleRef = ref(title);
+    const path = route.path;
+
+    titleMap.set(path, titleRef);
+
+    return titleRef;
 }
 
-export const useLayout = createSharedComposable(_useLayout);
+export function useLayout(options) {
+    const parent = inject(PLUGIN_LAYOUT_KEY, { reloadTab: () => void 0, closeTab: () => void 0 });
+    const titleRef = useTabTitle(options?.title);
+    return {
+        ...parent,
+        title: titleRef,
+    };
+}
