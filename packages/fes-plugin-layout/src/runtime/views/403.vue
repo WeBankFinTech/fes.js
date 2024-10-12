01b9b63a6bf66d0f46374868adf19b124dc6ac14
winixt <haizekuo@gmail.com>
2024-10-12 09:54:32 +0800

    feat: 403 404 页面文案可配置

--- packages/fes-plugin-layout/src/runtime/views/403.vue
+++ packages/fes-plugin-layout/src/runtime/views/403.vue
@@ -1,8 +1,10 @@
 <template>
-    <Wrapper :iconSrc="img403" title="没有访问权限，请联系管理人员" subTitle="" />
+    <Wrapper :icon-src="img403" :title="title" sub-title="" />
 </template>
+
 <script>
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
+import getConfig from '../helpers/getConfig';
 import img403 from '../assets/403.png';
 import Wrapper from './components/Wrapper.vue';
 
@@ -11,8 +13,13 @@ export default defineComponent({
         Wrapper,
     },
     setup() {
+        const config = getConfig();
+        const title = computed(() => {
+            return config['403']?.title || '没有访问权限，请联系管理人员';
+        });
         return {
             img403,
+            title,
         };
     },
 });
