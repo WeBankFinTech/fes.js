efd87087522cd2dce6b5dc89fbe93ef46efd08bf
qlin <haizekuo@gmail.com>
2024-11-22 10:29:48 +0800

    feat: webpack-dev 升级到5.x,提升 dev 性能,优化日志输出 (#259)

--- packages/fes-builder-webpack/src/plugins/commands/dev/connectHistoryMiddleware.js
+++ packages/fes-builder-webpack/src/plugins/commands/dev/connectHistoryMiddleware.js
@@ -1,19 +1,21 @@
-import { extname } from 'path';
+import { extname } from 'node:path';
 import historyFallback from 'connect-history-api-fallback';
 
 const ASSET_EXT_NAMES = ['.ico', '.png', '.jpg', '.jpeg', '.gif', '.svg'];
 
-const proxyMiddleware = (api) => (req, res, next) => {
-    const proxyConfig = api.config.proxy;
-    if (proxyConfig && Object.keys(proxyConfig).some((path) => req.url.startsWith(path))) {
-        return next();
-    }
-    if (ASSET_EXT_NAMES.includes(extname(req.url))) {
-        return next();
-    }
+function proxyMiddleware(api) {
+    return (req, res, next) => {
+        const proxyConfig = api.config.proxy;
+        if (proxyConfig && Object.keys(proxyConfig).some(path => req.url.startsWith(path))) {
+            return next();
+        }
+        if (ASSET_EXT_NAMES.includes(extname(req.url))) {
+            return next();
+        }
 
-    const history = historyFallback();
-    history(req, res, next);
-};
+        const history = historyFallback();
+        history(req, res, next);
+    };
+}
 
 export default proxyMiddleware;
