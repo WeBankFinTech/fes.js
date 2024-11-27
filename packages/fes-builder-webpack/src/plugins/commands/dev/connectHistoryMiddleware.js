7c01ee979e0776101c0f00bebca8a283d48cef6e
qlin <haizekuo@gmail.com>
2024-11-27 11:31:20 +0800

    fix: 代理插件兼容 array 写法 (#260)

--- packages/fes-builder-webpack/src/plugins/commands/dev/connectHistoryMiddleware.js
+++ packages/fes-builder-webpack/src/plugins/commands/dev/connectHistoryMiddleware.js
@@ -6,8 +6,15 @@ const ASSET_EXT_NAMES = ['.ico', '.png', '.jpg', '.jpeg', '.gif', '.svg'];
 function proxyMiddleware(api) {
     return (req, res, next) => {
         const proxyConfig = api.config.proxy;
-        if (proxyConfig && Object.keys(proxyConfig).some(path => req.url.startsWith(path))) {
-            return next();
+        if (proxyConfig) {
+            if (Array.isArray(proxyConfig)) {
+                if (proxyConfig.some(item => item.context.some(path => path && req.url.startsWith(path)))) {
+                    return next();
+                }
+            }
+            else if (Object.keys(proxyConfig).some(path => req.url.startsWith(path))) {
+                return next();
+            }
         }
         if (ASSET_EXT_NAMES.includes(extname(req.url))) {
             return next();
