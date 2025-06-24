bc715542cee638a7825129ce0d8f002346705af7
winixt <haizekuo@gmail.com>
2025-06-24 15:12:44 +0800

    fix: upgrade app

--- packages/create-fes-app/templates/app/h5/.fes.js
+++ packages/create-fes-app/templates/app/h5/.fes.js
@@ -1,35 +1,34 @@
 // fes.config.js 只负责管理 cli 相关的配置
-import { defineBuildConfig } from '@fesjs/fes';
-import postcsspxtoviewport8plugin from 'postcss-px-to-viewport-8-plugin';
-
+import { defineBuildConfig } from '@fesjs/fes'
+import postcsspxtoviewport8plugin from 'postcss-px-to-viewport-8-plugin'
 
 export default defineBuildConfig({
-    define: {
-        // __VUE_OPTIONS_API__: true,
-        // __VUE_PROD_DEVTOOLS__: false
-    },
-    html: {
-        title: '拉夫德鲁',
-    },
-    targets: {
-        chrome: '61',
-        ios: '10',
-    },
-    extraPostCSSPlugins: [
-        postcsspxtoviewport8plugin({
-            unitToConvert: 'px',
-            viewportWidth: 375,
-            unitPrecision: 5,
-            propList: ['*'],
-            viewportUnit: 'vw',
-            fontViewportUnit: 'vw',
-            selectorBlackList: [],
-            minPixelValue: 1,
-            mediaQuery: false,
-            replace: true,
-            exclude: [],
-            landscape: false,
-            landscapeUnit: 'vw',
-        }),
-    ],
-});
+  define: {
+    // __VUE_OPTIONS_API__: true,
+    // __VUE_PROD_DEVTOOLS__: false
+  },
+  html: {
+    title: '拉夫德鲁',
+  },
+  targets: {
+    chrome: '61',
+    ios: '10',
+  },
+  extraPostCSSPlugins: [
+    postcsspxtoviewport8plugin({
+      unitToConvert: 'px',
+      viewportWidth: 375,
+      unitPrecision: 5,
+      propList: ['*'],
+      viewportUnit: 'vw',
+      fontViewportUnit: 'vw',
+      selectorBlackList: [],
+      minPixelValue: 1,
+      mediaQuery: false,
+      replace: true,
+      exclude: [],
+      landscape: false,
+      landscapeUnit: 'vw',
+    }),
+  ],
+})
--- packages/create-fes-app/templates/app/h5/.prettierrc.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    ...require("@webank/eslint-config-webank/.prettierrc.js"),
-};
