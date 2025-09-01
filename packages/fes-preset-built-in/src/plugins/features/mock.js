6ae0b0a75cb8d4b3433d8c1c0f5fcac378c313f0
username <you@example.com>
2025-09-01 22:00:24 +0800

    fix: mock.js request method match

--- packages/fes-preset-built-in/src/plugins/features/mock.js
+++ packages/fes-preset-built-in/src/plugins/features/mock.js
@@ -137,7 +137,7 @@ export default (api) => {
                 return next();
             }
             // 请求以 cgiMock.prefix 开头，匹配处理
-            const matchRequet = requestList.find((item) => req.path.search(item.url) !== -1);
+            const matchRequet = requestList.find((item) => req.path.search(item.url) !== -1 && req.method === (item.method || 'GET').toUpperCase());
             if (!matchRequet) {
                 return next();
             }
